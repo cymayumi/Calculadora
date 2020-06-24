da5c58a713521127f037e49d40eea62912e954de
Cynthia Oka <cynthia.mosilva@gmail.com>
2020-06-24 11:17:18 -0300

    Layout ok

---
+++ app/src/main/java/com/mayumi/calculadora/CalculadoraActivity.kt
@@ -0,0 +1,11 @@
+package com.mayumi.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculadoraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora)
+    }
+}
